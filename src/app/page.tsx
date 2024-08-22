d53113f053ea817ecfb74abfee9f32c13a18e8d5
BrunoLisboa <brlisbo@gmail.com>
2024-08-22 12:01:00 -0300

    fix added suspense

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 import { MapContainer } from "@/components";
 
 import countriesJSON from "@/data/countries.json";
+import { Suspense } from "react";
 
 export default function Home() {
   return (
     <main className="flex h-screen m-auto flex-col justify-between items-center px-2 py-4 gap-4 w-full md:w-[900px]">
-      <MapContainer locations={countriesJSON} />
+      <Suspense fallback={<div>Cargando...</div>}>
+        <MapContainer locations={countriesJSON} />
+      </Suspense>
     </main>
   );
 }
